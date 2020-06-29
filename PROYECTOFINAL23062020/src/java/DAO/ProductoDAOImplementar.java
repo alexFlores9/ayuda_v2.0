494d7f7c3635be3e2a26e4e60f37f26ba522a18a
oscarnoelopezceron <61834003+oscarnoelopezceron@users.noreply.github.com>
2020-06-29 10:42:28 -0600

    :( Producto con Variables de tipo Date

--- PROYECTOFINAL23062020/src/java/DAO/ProductoDAOImplementar.java
+++ PROYECTOFINAL23062020/src/java/DAO/ProductoDAOImplementar.java
@@ -1,49 +1,56 @@
 package DAO;
 
+import Model.*;
+import java.util.List;
 import Factory.ConexionBD;
 import Factory.FactoryConexionBD;
-import Model.Producto;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.List;
 
+/**
+ *
+ * @author PC
+ */
 public class ProductoDAOImplementar implements ProductoDAO{
-    
-    ConexionBD conn;  //Crear el objeto tipo conexión.
-
+    ConexionBD conn;//La variable conexion
     public ProductoDAOImplementar() {
-        //Definir a la base de datos que se conectará por defecto.
+        //la conexionpara la definicion de la bd que s econectara por defecto
         //this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
-    }
         
-   
+    }
+
     @Override
     public List<Producto> Listar() {
         this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
+        
         StringBuilder miSQL = new StringBuilder();
-        miSQL.append("SELECT * FROM tb_producto;");
+        miSQL.append("SELECT * FROM tb_producto; ");
         List<Producto> lista = new ArrayList<Producto>();
         try{
-            //Se crea el objeto ResultSet implementando el método (consultaSQL) creado para la consulta.
             ResultSet resultadoSQL = this.conn.consultaSQL(miSQL.toString());
             while(resultadoSQL.next()){
                 Producto producto = new Producto();
-                //Asignar cada campo consultado al atributo en la clase.
+                Categoria c = new Categoria();
                 producto.setId_producto(resultadoSQL.getInt("id_producto"));
                 producto.setNom_producto(resultadoSQL.getString("nom_producto"));
+                producto.setStock(resultadoSQL.getDouble("stock"));
+                producto.setPrecio(resultadoSQL.getDouble("precio"));
+                producto.setUnidad_de_medida(resultadoSQL.getString("unidad_de_medida"));
+                producto.setEstado_producto(resultadoSQL.getInt("estado_producto"));
+                c.setNom_categoria(resultadoSQL.getString("nom_categoria"));
+                producto.setDes_producto(resultadoSQL.getString("des_producto"));
+                producto.setFecha_entrada(resultadoSQL.getDate("fecha_entrada"));
+                producto.setCategoria(c);
+                lista.add(producto);
                 
-                lista.add(producto); //Agregar al array cada registro encontrado.
             }
         }catch(Exception ex){
-            
         }finally{
             this.conn.cerrarConexion();
         }
-        
         return lista;
     }
 
-    
     @Override
     public List<Producto> Listar2() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -53,77 +60,39 @@ public class ProductoDAOImplementar implements ProductoDAO{
     public Producto editarPro(int id_pro_edit) {
         this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
         Producto producto = new Producto();
+        Categoria c = new Categoria();
         StringBuilder miSQL = new StringBuilder();
-        //Agregar la consulta SQL.
+        
         miSQL.append("SELECT * FROM tb_producto WHERE id_producto = ").append(id_pro_edit);
-        //Realizar la consulta.
         try{
             ResultSet resultadoSQL = this.conn.consultaSQL(miSQL.toString());
             while(resultadoSQL.next()){
-                producto.setId_producto(resultadoSQL.getInt("id_producto"));
-                producto.setNom_producto(resultadoSQL.getString("nom_producto"));
-                
+                 producto.setId_producto(resultadoSQL.getInt("id_producto"));
+                 producto.setNom_producto(resultadoSQL.getString("nom_producto"));
+                 producto.setStock(resultadoSQL.getDouble("stock"));
+                 producto.setPrecio(resultadoSQL.getDouble("precio"));
+                 producto.setUnidad_de_medida(resultadoSQL.getString("unidad_de_medida"));
+                 producto.setEstado_producto(resultadoSQL.getInt("estado_producto"));
+                 c.setNom_categoria(resultadoSQL.getString("nom_categoria"));
+                 producto.setDes_producto(resultadoSQL.getString("des_producto"));
+                 producto.setFecha_entrada(resultadoSQL.getDate("fecha_entrada"));
             }
         }catch(Exception e){
         }finally{
             this.conn.cerrarConexion();
         }
-        
         return producto;
     }
 
     @Override
     public boolean guardarPro(Producto producto) {
-        this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
-        boolean guardar = false;
-        try{
-            if(producto.getId_producto()== 0){
-                StringBuilder miSQL = new StringBuilder();
-                //Agregar consulta SQL; el id_producto es autoincrementable.
-                miSQL.append("INSERT INTO tb_producto(nom_producto) VALUES ('");
-                miSQL.append(producto.getNom_producto());
-                miSQL.append(");");
-                //Invocar método para ejecutar la consulta.
-                this.conn.ejecutarSQL(miSQL.toString());
-                System.out.println("Registro Guardado...");
-            }else if(producto.getId_producto()>0){                            //Comprobación para actualizar...
-                System.out.println("Entramos...");
-                StringBuilder miSQL = new StringBuilder();
-                miSQL.append("UPDATE tb_producto SET id_producto = ").append(producto.getId_producto());
-                miSQL.append(", nom_producto =  '").append(producto.getNom_producto());
-                
-                miSQL.append(" WHERE id_producto = ").append(producto.getId_producto()).append(";");
-                //Invocar método para ejecutar la consulta.
-                this.conn.ejecutarSQL(miSQL.toString());
-                System.out.println("Registro modificado correctamente!");
-            }
-            
-          //return guardar;
-        }catch(Exception e){
-            
-        }finally{
-            this.conn.cerrarConexion();
-        }
-        return guardar;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean borrarPro(int id_pro_borrar) {
-        this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
-        boolean borrar = false;           //Bandera de resultados
-        try{
-            StringBuilder miSQL = new StringBuilder();
-            miSQL.append("DELETE FROM tb_producto WHERE id_producto = ").append(id_pro_borrar);
-            this.conn.ejecutarSQL(miSQL.toString());
-            borrar = true;
-        }catch(Exception e){
-            
-        }finally{
-            this.conn.cerrarConexion();  //Cerrar la conexión.
-        }
-        return borrar;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    
-    
 }
+
